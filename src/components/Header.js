0dc294b91775a3de5da8bb382ca26d816247595e
kevin-sharp24 <kevin.sharp24@gmail.com>
2022-10-12 21:16:34 -0500

    commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header>
+            <div className="content-fit-width">
+                <h1>Find your film</h1>
+                <p id="header--nav-link">My Watchlist</p>
+            </div>
+        </header>
+    )
+}
---
+++ src/components/WatchlistPage.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function WatchlistPage() {
+    return (
+        <main id="watchlist-page">
+            <h2>Watchlist Page</h2>
+        </main>
+    )
+}
