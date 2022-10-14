ef74aa1200eae76e5c65fe768c3b7cc6e55a19fe
kevin-sharp24 <kevin.sharp24@gmail.com>
2022-10-14 16:03:36 -0500

    header link added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,24 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 export default function Header() {
+    const [onSearchPage, setOnSearchPage] = React.useState(true)
+
+    function handleClick() {
+        setOnSearchPage(onSearchPage => !onSearchPage)
+    }
+
     return (
         <header>
             <div className="content-fit-width">
                 <h1>Find your film</h1>
-                <p id="header--nav-link">My Watchlist</p>
+                <Link 
+                    id="header--nav-link" 
+                    to={onSearchPage ? "/watchlist" : "/"}
+                    onClick={handleClick}
+                >
+                    {onSearchPage ? "My Watchlist" : "Search for movies"}
+                </Link>
             </div>
         </header>
     )
